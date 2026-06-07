06f16f300c4cb46713f9fde7c0f83fcad7fd3900
diogovicente8c-commits <diogovicente8c@gmail.com>
2026-06-07 16:46:08 +0100

    Update App_scorer.py

--- App_scorer.py
+++ App_scorer.py
@@ -8,7 +8,7 @@ from requests_ratelimiter import LimiterSession
 
 warnings.filterwarnings("ignore")
 
-session = LimiterSession(per_second=2)
+session = LimiterSession(per_second=5)
 
 # Configuração da página para telemóvel
 st.set_page_config(page_title="Stock Scorer", page_icon="📈", layout="centered")
